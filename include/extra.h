e24f96bba88bebaf2d0b9b8e40965dbe8afc73b1
Julius De Baaij <jde-baai@f1r1s17.codam.nl>
2023-09-03 19:50:09 +0200

    array_len

--- include/extra.h
+++ include/extra.h
@@ -6,7 +6,7 @@
 /*   By: juliusdebaaij <juliusdebaaij@student.co      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/28 21:10:20 by juliusdebaa   #+#    #+#                 */
-/*   Updated: 2023/07/28 21:11:07 by juliusdebaa   ########   odam.nl         */
+/*   Updated: 2023/09/03 19:49:23 by jde-baai      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@
 void		ft_free_array(char **str);
 char		**ft_dup_array(char **str);
 void		*ft_realloc(void *ptr, size_t size);
+size_t		array_len(char **array);
 
 #endif /* EXTRA_H */
