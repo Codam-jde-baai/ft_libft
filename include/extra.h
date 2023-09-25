c7da1519baf19e0e4674272ec64e381e92f6f1bb
JuliusdB <julius@debaaij.com>
2023-09-25 20:25:33 +0200

    fixing header

--- include/extra.h
+++ include/extra.h
@@ -6,18 +6,19 @@
 /*   By: juliusdebaaij <juliusdebaaij@student.co      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/28 21:10:20 by juliusdebaa   #+#    #+#                 */
-/*   Updated: 2023/09/25 20:20:32 by juliusdebaa   ########   odam.nl         */
+/*   Updated: 2023/09/25 20:24:39 by juliusdebaa   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXTRA_H
-#define EXTRA_H
+# define EXTRA_H
 
-#include "libft.h"
-#include <stdlib.h>
+# include "libft.h"
+# include <stdlib.h>
 
-void ft_free_array(char **str);
-char **ft_dup_array(char **str);
-void *ft_realloc(void *ptr, size_t size);
+void	ft_free_array(char **str);
+char	**ft_dup_array(char **str);
+void	*ft_realloc(void *ptr, size_t size);
+size_t	array_len(char **array);
 
 #endif /* EXTRA_H */
